9abcd9412b5a817ee8c3ff2610df4dda16079dd8
Gage Peterson <justgage@gmail.com>
2013-07-24 19:14:55 -0600

    Made some starting files

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+   <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+   <title>ButterflyOils.com</title>
+   <link rel="stylesheet" href="style.css" type="text/css" media="screen" charset="utf-8">
+
+</head>
+<body>
+<div id="wrapper">
+
+
+
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'include/header.php'; ?>
+<div id="content">
+   <h1>This is the content</h1>
+   <p>This is the first paragraph</p>
+</div>
+
+<?php include 'include/footer.php'; ?>
