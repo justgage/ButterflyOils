60307312e37958f488fca66303c0c3884f62ba8d
Gage Peterson <justgage@gmail.com>
2013-08-08 10:13:29 -0600

    Added a lot to the backend started on auto oils

--- include/functions.php
+++ include/functions.php
@@ -1,6 +1,7 @@
 <?php
 //this will create a constant to the base directory.
 define("SITEURL", "http://localhost/butterfly/");
+include_once('admin/lib/markdown.php');
 
 class database {
 
@@ -13,9 +14,8 @@ class database {
       function __construct() {
          try {
             $this->db = new PDO($this->dsn, $this->username, $this->pass);
-            echo 'connected!';
          } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
+            echo 'ERROR:Connection failed: ' . $e->getMessage();
          }
       }
 
@@ -30,7 +30,41 @@ class database {
 
          return $result;
       }
+      public function display_oils()
+      {
+         $sql = 'SELECT * FROM oils';
+
+         $query = $this->db->prepare($sql);
+         $query->execute();
+
+         $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+         foreach ($result as $oils) {
+
+            $id = $oils['id'];
+            $name = $oils['name'];
+            $desciption = Markdown($oils['desciption']);
+            $price = $oils['price'];
+            $time = $oils['time'];
+            $visible = $oils['visible'];
+
+            $output = "";
+            $output .= <<<EOF
+<div class="product">
+<h3>$name</h3>
+<span class='price'>\$$price</span>
+<img src='img/bottle.jpg' />
+<p>$desciption</p>
+</div>
+
+EOF;
+            echo $output;
+            
+         }
+         echo "<br class='float-fix' />";
+      }
 }
 
 $db = new database();
+
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php include 'include/header.php'; ?>
 <div id="content">
    <!-- this is just some dummy code -->
-   <h1>This is a title</h1>
-   <h2>This is a subtitle</h1>
-   <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec urna tortor, suscipit at nisl et, bibendum rhoncus leo. Sed viverra, mauris eu consectetur suscipit, nibh quam iaculis massa, at aliquam dui nibh non odio. Pellentesque consequat laoreet sem. Curabitur eget augue vitae nisl tempus ultrices. Ut luctus molestie nibh, ut lacinia urna vehicula et. Fusce ac erat aliquam, interdum dui sit amet, malesuada quam. Nam auctor porta nulla ac fermentum. Aenean at pharetra nisl.</p>
-   <p>Duis nibh leo, adipiscing tincidunt tincidunt eget, sagittis at nulla. Pellentesque a arcu a nisi convallis sollicitudin eu vel massa. Ut ornare imperdiet mattis. Suspendisse dignissim dolor sed sem varius vulputate. Suspendisse potenti. Quisque in purus dignissim, eleifend orci a, venenatis augue. Donec hendrerit aliquet tellus, in hendrerit quam aliquam ac. Morbi pellentesque ligula quam, id accumsan nisi dictum vel. Duis neque velit, vulputate in lacinia eget, gravida at risus. Pellentesque tincidunt mi orci, in iaculis dui vestibulum eget. Fusce venenatis dui sed est luctus, vitae sagittis arcu semper. Maecenas interdum molestie mauris, a porta mauris laoreet in.</p>
-   <h2>Another Point</h1>
-   <p>Curabitur egestas urna in tellus fermentum, quis tincidunt justo lacinia. Fusce blandit arcu quis lectus tincidunt luctus. Quisque pulvinar quis tellus quis sagittis. Quisque sed tellus varius, accumsan orci lobortis, placerat lectus. Proin blandit, mi ut rhoncus imperdiet, lectus nunc rutrum neque, vel pretium metus lorem tristique ante. Morbi ultricies id augue at rutrum. Sed dictum vel quam ac hendrerit. Mauris id erat sed ante varius lacinia eget sit amet augue. Maecenas odio lectus, faucibus id ligula vitae, elementum rhoncus nunc. Etiam varius ligula eget nunc consectetur, at dapibus turpis aliquam. Morbi consequat condimentum viverra. </p>
+<?php $db->display_oils(); ?>
 </div>
 
 <?php include 'include/footer.php'; ?>
